cae493c7755c28fcb37a06914c4fa70b1b3cccca
LeYASSOUNG <diarrassoubayassoungo33@gmail.com>
2026-07-16 16:57:52 +0000

    Remove unused scratch scripts

--- ya-consulting-app/create_user.php
+++
@@ -1,8 +0,0 @@
-
-$u = App\Models\User::firstOrCreate(
-    ['email' => 'diarrassoubayassoungo33@gmail.com'],
-    ['name' => 'Diarrassouba', 'password' => Hash::make('150703')]
-);
-$u->assignRole('admin');
-echo 'OK';
-
